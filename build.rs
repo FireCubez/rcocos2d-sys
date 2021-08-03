3ae392036db5b63ed0bbea91e129e63188f7dcb6
FireCubez <firecubeez@gmail.com>
2021-08-03 03:04:35 +0200

    absolute path

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
 fn main() {
-	println!("cargo:rustc-link-search=cocos-headers/cocos2dx");
+	println!("cargo:rustc-link-search={}/cocos-headers/cocos2dx", std::env::var("CARGO_MANIFEST_DIR").unwrap());
 	println!("cargo:rustc-link-lib=libcocos2d");
 }
